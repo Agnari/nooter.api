af8aab89cc992aa93655b8ffe01a39047fc98fa7
Nympherr <lukasstauskas@yahoo.com>
2022-10-13 01:35:47 +0300

    added identity for database
    Įdėtas identity. Duombazė susidarė

---
+++ Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Models
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
