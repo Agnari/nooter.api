bc2837b0122ae3289f92fa78e266d44995a7c32f
Agnari <agatinapa@gmail.com>
2025-09-16 15:42:42 +0300

    GET all users method added

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Nooter.API.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Nooter.API.Controllers
 {
@@ -12,12 +14,14 @@ namespace Nooter.API.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly AppIdentityDbContext _db;
-        public UsersController(
-           UserManager<AppUser> userManager, AppIdentityDbContext db)
+
+        public UsersController(UserManager<AppUser> userManager, AppIdentityDbContext db)
         {
             _userManager = userManager;
             _db = db;
         }
+
+        // POST: api/users
         [HttpPost]
         public async Task<IActionResult> Register(RegisterModel model)
         {
@@ -30,8 +34,23 @@ namespace Nooter.API.Controllers
             if (!result.Succeeded)
                 return BadRequest(result);
 
-
             return Ok(user);
+        }
+
+        // GET: api/users
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = _userManager.Users
+                .Select(u => new
+                {
+                    u.Id,
+                    u.UserName,
+                    u.Email
+                })
+                .ToList();
+
+            return Ok(users);
         }
     }
 }
